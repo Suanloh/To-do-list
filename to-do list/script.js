e7d6ca2cc2e20271ff491686328dcb6da2dca903
Zisuanlo <zixuan325@student.usm.my>
2025-10-26 13:23:23 +0800

    Refactor task management functions in script.js

--- to-do list/script.js
+++ to-do list/script.js
@@ -7,15 +7,14 @@ function addTask(){
     
     }
     else{
-//newline        
         let li= document.createElement("li");
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
-//delete
+        //delete
         let span= document.createElement("span");
         span.innerHTML="\u00d7";
         li.appendChild(span);
-//edit
+        //edit
         let editBtn= document.createElement("button");
         editBtn.innerHTML= "\u002B";
         editBtn.classList.add("editBtn");
@@ -43,13 +42,15 @@ function addTask(){
  function showTask(){
     listContainer.innerHTML= localStorage.getItem("data");
  }
-
  showTask();
 
-function editTask(li){
+//  startchange here
+ function editTask(li){
     const currentText= li.firstChild.textContent;
     const newText= prompt("Edit yout task: ", currentText);
     if(newText && newText.trim()!== ""){
         li.firstChild.textContent= newText;
         saveData();
     }
+ }
+ 
