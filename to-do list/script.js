c1410e41d8e6a70ad6c5a114dc0ddd8159eb3c79
Zisuanlo <zixuan325@student.usm.my>
2025-10-26 13:17:45 +0800

    Update script.js

--- to-do list/script.js
+++ to-do list/script.js
@@ -7,13 +7,20 @@ function addTask(){
     
     }
     else{
+//newline        
         let li= document.createElement("li");
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
-
+//delete
         let span= document.createElement("span");
         span.innerHTML="\u00d7";
         li.appendChild(span);
+//edit
+        let editBtn= document.createElement("button");
+        editBtn.innerHTML= "\u002B";
+        editBtn.classList.add("editBtn");
+        editBtn.onclick = () => editTask(li); // to the edit function
+        li.appendChild(editBtn);
     }
     inputBox.value="";
     saveData();
@@ -36,4 +43,13 @@ function addTask(){
  function showTask(){
     listContainer.innerHTML= localStorage.getItem("data");
  }
+
  showTask();
+
+function editTask(li){
+    const currentText= li.firstChild.textContent;
+    const newText= prompt("Edit yout task: ", currentText);
+    if(newText && newText.trim()!== ""){
+        li.firstChild.textContent= newText;
+        saveData();
+    }
